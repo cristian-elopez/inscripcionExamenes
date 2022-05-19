d37e4a1c6576de685f4825784d318904a1d053fe
Cristian Lopez <cristianelopez05@gmail.com>
2022-05-19 17:33:38 -0300

    segundo commit

--- code.js
+++ code.js
@@ -1,8 +1,38 @@
-const sendButton = document.getElementById('enviar-datos');
-
+// seleccionar boton de confirmar
+const sendButton = document.querySelector('.enviar-datos');
+// aparecer modal
 sendButton.addEventListener("click",()=>{
     document.querySelector(".mensaje").innerHTML.value;
     let modal = document.querySelector(".modal-background");
     modal.style.display = "flex";
     modal.style.animation = "aparecer 1s forwards";
 });
+
+// aparecer el boton para ir arriba
+window.onscroll = function(){
+    if (document.documentElement.scrollTop > 100) {
+        let contenedor = document.querySelector(".go-top-container");
+        contenedor.classList.add("show");
+    } else {
+        let contenedor = document.querySelector(".go-top-container");
+        contenedor.classList.remove("show");
+    };
+};
+// evento para subir
+let contenedor = document.querySelector(".go-top-container");
+contenedor.addEventListener("click",()=>{
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+
+// prueba scroll hasta el formulario
+// let irAform = document.querySelector(".go-to-mesas");
+// let formMesas = document.querySelector("titulo-h2");
+// irAform.addEventListener("click",()=>{
+//     window.scrollTo(formMesas,{
+//         behavior: "smooth"
+//     });
+// });
